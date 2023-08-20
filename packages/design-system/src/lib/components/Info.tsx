a921363aa82035f6e6eba5c12ba0454cfcf3ee0d
Christina Yang <ychristina011@gmail.com>
2023-08-20 10:40:14 -0700

    move info, separator, tooltip

---
+++ packages/design-system/src/lib/components/Info.tsx
@@ -0,0 +1,14 @@
+"use client"
+/**
+ * Author: Christina Yang
+ */
+import { Tooltip } from './Tooltip'
+import { InfoCircledIcon } from '@radix-ui/react-icons'
+
+export const Info = ({ text, style }: { text: string, style?: any }) => {
+    return (
+        <Tooltip content={text}>
+            <InfoCircledIcon style={{ transformOrigin: 'center', width: 'max-content', display: 'flex-inline', justifyContent: 'center', alignItems: 'center', verticalAlign: 'middle', ...style }} />
+        </Tooltip>
+    )
+}
