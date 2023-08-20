6f3be146cfeb56abe9daea2baa93214eb0021410
Christina Yang <ychristina011@gmail.com>
2023-08-19 17:22:02 -0700

    idk what i'm doing

--- packages/design-system/vite.config.ts
+++ packages/design-system/vite.config.ts
@@ -1,3 +1,4 @@
+import { macaronVitePlugin } from '@macaron-css/vite';
 import react from '@vitejs/plugin-react';
 import path from 'node:path';
 import { defineConfig } from 'vite';
@@ -9,6 +10,7 @@ export default defineConfig({
         dts({
             insertTypesEntry: true,
         }),
+        macaronVitePlugin(),
     ],
     build: {
         lib: {
